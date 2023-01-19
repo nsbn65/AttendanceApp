8bb0bd737ee3e8b7f37eb14fbc48a5ab997d3ed4
nsbn65 <saito12pbyf8o@gmail.com>
2023-01-19 16:35:08 +0900

    attendance-app

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attendance;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Attendance;
+use Illuminate\Support\Facades\Auth;
 
 
 class AttendanceController extends Controller
@@ -23,10 +22,41 @@ class AttendanceController extends Controller
     public function punchIn()
     {
         $user = Auth::user();
-        $timestamp = Timestamp::create([
+        $oldAttendance = Attendance::where('user_id', $user->id)->latest()->first();
+        if ($oldAttendance) {
+            $oldAttendancePunchIn = new Carbon($oldAttendance->punchIn);
+            $oldAttendanceDay = $oldAttendancePunchIn->startOfDay();
+        }
+
+        $newAttendanceDay = Carbon::today();
+
+        /**
+         * 日付を比較する。同日付の出勤打刻で、かつ直前のTimestampの退勤打刻がされていない場合エラーを吐き出す。
+         */
+        if (($oldAttendanceDay == $newAttendanceDay) && (empty($oldAttendanceDay->punchOut))){
+            return redirect()->back()->with('error', 'すでに出勤打刻がされています');
+        }
+
+        $attendance = Attendance::create([
             'user_id' => $user->id,
-            'punchIn' => Carbon::now()
+            'start_time' => Carbon::now(),
         ]);
-        return redirect()->back();
+
+        return redirect()->back()->with('my_status', '出勤打刻が完了しました');
+    }
+
+    public function punchOut() 
+    {
+        $user = Auth::user();
+        $attendance = Attendance::where('user_id', $user->id)->latest()->first();
+
+        if(!empty($timestamp->punchOut)) {
+            return redirect()->back()->with('error', '既に退勤の打刻がされているか、出勤打刻されていません');
+        }
+        $attendance->update([
+            'end_time' => Carbon::now()
+        ]);
+
+        return redirect()->back()->with('my_status', '退勤打刻が完了しました');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,14 @@ use App\Http\Controllers\RecordController;
 
 
 Route::get('/',[AttendanceController::class,'index']);
-
 Route::post('/punch/in',[AttendanceController::class,'punchIn'])->name('punch/in');
 Route::post('/punch/out',[AttendanceController::class,'punchOut'])->name('punch/out');
-
 Route::post('/rest/in',[RestController::class,'restIn'])->name('rest/in');
 Route::post('/rest/out',[RestController::class,'restOut'])->name('rest/out');
-
 Route::get('/record',[RecordController::class,'index']);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
