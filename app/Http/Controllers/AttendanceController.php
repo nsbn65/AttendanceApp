d2c32c2d7706841df4735d563ba3eaade586a731
nsbn65 <saito12pbyf8o@gmail.com>
2023-01-20 16:45:21 +0900

    attendance-app

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -22,41 +22,19 @@ class AttendanceController extends Controller
     public function punchIn()
     {
         $user = Auth::user();
-        $oldAttendance = Attendance::where('user_id', $user->id)->latest()->first();
-        if ($oldAttendance) {
-            $oldAttendancePunchIn = new Carbon($oldAttendance->punchIn);
-            $oldAttendanceDay = $oldAttendancePunchIn->startOfDay();
-        }
         
-        $newAttendanceDay = Carbon::today();
-
-        /**
-         * 日付を比較する。同日付の出勤打刻で、かつ直前のTimestampの退勤打刻がされていない場合エラーを吐き出す。
-         */
-        if (($oldAttendanceDay == $newAttendanceDay) && (empty($oldAttendanceDay->punchOut))){
-            return redirect()->back()->with('error', 'すでに出勤打刻がされています');
-        }
-
-        $attendance = Attendance::create([
-            'user_id' => $user->id,
-            'start_time' => Carbon::now(),
-        ]);
-
-        return redirect()->back()->with('my_status', '出勤打刻が完了しました');
+        $start_time = Carbon::now();
+        //dd($start_time);
+        return redirect('/');
     }
+
 
     public function punchOut() 
     {
         $user = Auth::user();
-        $attendance = Attendance::where('user_id', $user->id)->latest()->first();
-
-        if(!empty($attendance->punchOut)) {
-            return redirect()->back()->with('error', '既に退勤の打刻がされているか、出勤打刻されていません');
-        }
-        $attendance->update([
-            'end_time' => Carbon::now()
-        ]);
-
-        return redirect()->back()->with('my_status', '退勤打刻が完了しました');
+        
+        $end_time = Carbon::now();
+        //dd($end_time);
+        return redirect('/');
     }
 }
--- app/Http/Controllers/RestContoller.php
+++ app/Http/Controllers/RestContoller.php
@@ -8,5 +8,21 @@ use Illuminate\Support\Facades\Auth;
 
 class RestContoller extends Controller
 {
-    //
+    public function restIn()
+    {
+        $user = Auth::user();
+        
+        $start_rest_time = Carbon::now();
+        //dd($start_rest_time);
+        return redirect('/');
+    }
+
+    public function restOut()
+    {
+        $user = Auth::user();
+        
+        $end_rest_time = Carbon::now();
+        //dd($end_rest_time);
+        return redirect('/');
+    }
 }
